b4dbb38dc808cb8c800af74f6114160abf658a83
RocioDelAlb4 <rocio.of.alba@gmail.com>
2024-04-15 23:44:23 -0400

    modulos pagina principal

--- evaLS/src/components/main/Module.tsx
+++ evaLS/src/components/main/Module.tsx
@@ -1,6 +1,33 @@
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Image } from "@nextui-org/react"
+import { Link } from "react-router-dom"
+import { ModuleI } from "../../interfaces/ModuleI"
 
-export const Module = () => {
+export const Module = ({ data }: { data: ModuleI }) => {
   return (
-    <div>Module</div>
+    <Card className="max-w-[450px]">
+      <CardHeader className="flex justify-center">
+        <Image
+          alt="nextui logo"
+          radius="none"
+          height="100%"
+          src={ data.image }
+          width="100%"
+          
+        />
+      </CardHeader>
+      <Divider/>
+      <CardBody>
+        <h4 className="font-bold text-large">{ data.title }</h4>
+        <p>{ data.description }</p>
+      </CardBody>
+      <Divider/>
+      <CardFooter className="flex justify-end">
+      <Link to="/auth">
+        <Button color="primary" variant="flat" size="lg">
+          Iniciar
+        </Button>
+      </Link>
+      </CardFooter>
+    </Card>
   )
 }
--- evaLS/src/pages/main/MainPage.tsx
+++ evaLS/src/pages/main/MainPage.tsx
@@ -1,16 +1,57 @@
 import { Footer } from "../../components/main/Footer";
 import { Header } from "../../components/main/Header"
 import { Module } from '../../components/main/Module';
+import { ModuleI } from "../../interfaces/ModuleI";
+import uno from '../../assets/images/1.jpg';
+import dos from '../../assets/images/2.jpg';
+import tres from '../../assets/images/3.jpg';
+import cuatro from '../../assets/images/4.jpg';
+import repaso from '../../assets/images/repaso2.jpg';
+import desafio from '../../assets/images/desafio.jpg';
 
 export const MainPage = () => {
+  // const data: { title: string, description: string } = {title: '', description: ''};
+  const modules: ModuleI[] = [
+    { 
+      title: 'Modulo 1',
+      description: 'Este modulo sera una introducción y guía.',
+      image: uno
+    },
+    { 
+      title: 'Modulo 2',
+      description: 'Este modulo abarcará números.',
+      image: dos
+    },
+    { 
+      title: 'Modulo 3',
+      description: 'Este modulo abarcará letras.',
+      image: tres
+    },
+    { 
+      title: 'Modulo 4',
+      description: 'Este modulo abarcará palabras.',
+      image: cuatro
+    },
+    { 
+      title: 'Repaso',
+      description: 'Repasa las dudas que tengas.',
+      image: repaso
+    },
+    { 
+      title: 'Desafio ',
+      description: 'Desafiate a ti mismo con tests.',
+      image: desafio
+    },
+  ];
   return (
     <>
       <Header />
-      <div>
+      <div className="flex gap-8 flex-wrap">
+        {/* <Module module={data}/> */}
+        {/* <Module/>
         <Module/>
-        <Module/>
-        <Module/>
-        <Module/>
+        <Module/> */}
+        { modules.map((module) => <Module data={module}/>) };
       </div>
 
       <Footer/>
